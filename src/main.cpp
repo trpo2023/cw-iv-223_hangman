2385c14101373e713391bbaaa98101f96e18e17c
Timur Narziev <timan221@mail.ru>
2023-05-20 23:20:55 +0700

    Added Makefile

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,58 @@
 using namespace std;
 
 int main() {
-	setlocale(LC_ALL, "Russian");
-	srand(time(NULL));
-	pair<string, string> word[3];
-	word[0].first = "lamp";
-	word[0].second = "висит груша нельзя скушать? (напиши по английски)";
-	word[1].first = "elizabeth";
-	word[1].second = "Имя лучшего преподователя по ТРПО? (напиши по английски)";
-	word[2].first = "darkness";
-	word[2].second = "Чем больше этого есть, тем меньше вы можете видеть. О чем идет речь? (напиши по английски)";
-	int x = rand() % 3;
-	string wordToGuess = word[x].first; // слово для угадывания
-	string worddiscription = word[x].second; // зописание к слову
+    setlocale(LC_ALL, "Russian");
+    srand(time(NULL));
+    pair<string, string> word[3];
+    word[0].first = "lamp";
+    word[0].second = "висит груша нельзя скушать? (напиши по английски)";
+    word[1].first = "elizabeth";
+    word[1].second = "Имя лучшего преподователя по ТРПО? (напиши по английски)";
+    word[2].first = "darkness";
+    word[2].second = "Чем больше этого есть, тем меньше вы можете видеть. О чем идет речь? (напиши по английски)";
+    int x = rand() % 3;
 
-	switch (wrongGuesses) {
+    const int MAX_TRIES = 7; // максимальное количество попыток
+    int wrongGuesses = 0; // счетчик неправильных ответов
+    string wordToGuess = word[x].first; // слово для угадывания
+    string worddiscription = word[x].second;
+    string lettersGuessed = ""; // строка уже угаданных букв
+    bool gameWon = false; // флаг - игра выиграна или нет
+
+    cout << "Угадайте слово: " << endl;
+    cout << worddiscription << endl;
+    while (wrongGuesses < MAX_TRIES && !gameWon) {
+        for (int i = 0; i < wordToGuess.length(); i++) {
+            if (lettersGuessed.find(wordToGuess[i]) != string::npos)
+                cout << wordToGuess[i] << " ";
+            else
+                cout << "_ ";
+        }
+        cout << endl;
+
+        char guess;
+        cout << "Введите букву: ";
+        cin >> guess;
+
+        if (lettersGuessed.find(guess) != string::npos) {
+            cout << "Вы уже угадали эту букву!" << endl;
+            continue;
+        }
+
+        if (wordToGuess.find(guess) != string::npos) {
+            cout << "Верно!" << endl;
+            for (int i = 0; i < wordToGuess.size(); i++)
+                if (wordToGuess[i] == guess)
+                    lettersGuessed += guess;
+            if (lettersGuessed.length() == wordToGuess.length())
+                gameWon = true;
+        }
+        else {
+            cout << "Неправильно! :( " << endl;
+            wrongGuesses++;
+        }
+
+        switch (wrongGuesses) {
         case 1:
             cout << "   ___________" << endl;
             cout << "   |         |" << endl;
@@ -74,5 +112,12 @@ int main() {
             cout << "___|___" << endl;
             break;
         }
+    }
 
+    if (gameWon)
+        cout << "Поздравляем! Вы выиграли!" << endl << wordToGuess << endl;
+    else
+        cout << "Вы проиграли. Загаданное слово: " << wordToGuess << endl;
+
+    return 0;
 }
