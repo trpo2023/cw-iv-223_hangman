f10340c265d26b8609bb6e5f92851c2b54cafd40
LilChebyrek <QuantumChebyrek@gmail.com>
2023-10-05 01:30:21 +0700

    edit style agaim

--- src/main.c
+++ src/main.c
@@ -5,26 +5,26 @@
 #include <time.h>
 
 void gameLoop() {
-    while (1) {
-        system("clear");
-        int choice;
+  while (1) {
+    system("clear");
+    int choice;
 
-        fflush(stdout);
-        printf("\033[12;25H  Добро пожаловать в игру Виселица! \033[35;0H");
-        printf("Введите 1 для запуска программы или любую клавишу для "
-               "выхода: ");
-        scanf("%d", &choice);
-        if (choice == 1) {
-            // gamePlay();
-            break;
-        } else {
-            printf("Программа завершена\n");
-            break;
-        }
+    fflush(stdout);
+    printf("\033[12;25H  Добро пожаловать в игру Виселица! \033[35;0H");
+    printf("Введите 1 для запуска программы или любую клавишу для "
+           "выхода: ");
+    scanf("%d", &choice);
+    if (choice == 1) {
+      // gamePlay();
+      break;
+    } else {
+      printf("Программа завершена\n");
+      break;
     }
+  }
 }
 
 int main() {
-    gameLoop();
-    return 0;
+  gameLoop();
+  return 0;
 }
