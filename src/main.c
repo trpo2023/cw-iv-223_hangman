cf437e59628d0fa59588acf1b8d8a62b4d16b2f9
LilChebyrek <QuantumChebyrek@gmail.com>
2023-10-05 01:19:06 +0700

    edit codeStyle

--- src/main.c
+++ src/main.c
@@ -1,20 +1,18 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <ctype.h>
 
-
-
-void gameLoop(){
-    while (1)
-    {
+void gameLoop() {
+    while (1) {
         system("clear");
         int choice;
 
         fflush(stdout);
         printf("\033[12;25H  Добро пожаловать в игру Виселица! \033[35;0H");
-        printf("Введите 1 для запуска программы или любую клавишу для выхода: ");
+        printf("Введите 1 для запуска программы или любую клавишу для "
+               "выхода: ");
         scanf("%d", &choice);
         if (choice == 1) {
             // gamePlay();
@@ -26,7 +24,7 @@ void gameLoop(){
     }
 }
 
-int main(){
+int main() {
     gameLoop();
     return 0;
 }
